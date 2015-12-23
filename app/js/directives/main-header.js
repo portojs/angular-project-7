3d402b845b74e10d8c52606dd3c90640a549a180
peter <peter.k.nets@gmail.com>
2015-12-23 05:55:10 +0200

    Retain 'selected' status of nav buttons

--- app/js/directives/main-header.js
+++ app/js/directives/main-header.js
@@ -1,9 +1,33 @@
 'use strict';
 
 angular.module('swordsApp')
-  .directive('mainHeader', function() {
+  .directive('mainHeader', ['$location', function($location) {
     return {
       restrict: 'E',
-      templateUrl: 'templates/directives/main-header.html'
+      templateUrl: 'templates/directives/main-header.html',
+      link: function(scope, element, attrs) {
+        scope.checkSelected = function(check) {
+          return check === scope.currentlySelected;
+        };
+        scope.makeSelected = function(select) {
+          scope.currentlySelected = select;
+        };
+        scope.compareSelected = function() {
+          if ($location.path() === '/main') {
+            return 1;
+          } else if ($location.path() === '/catalogue') {
+            return 2;
+          } else if ($location.path() === '/sign_up') {
+            return 3;
+          } else if ($location.path() === '/contact') {
+            return 4;
+          } else if ($location.path() === '/private_cabinet') {
+            return 5;
+          } else {
+            return 1;
+          }
+        };
+        scope.currentlySelected = scope.compareSelected();
+      }
     };
-  });
+  }]);
